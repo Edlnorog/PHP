298025dc31629e0b4b40575aeb46274ab096d985
Кирилл Якунин <alduin20060426@gmail.com>
2025-02-15 00:25:37 +0300

    hm1

---
+++ Homework/hm1/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <img src="logo.png" alt="" width= 200px >
+        <h1>Hello, World!</h1>
+    </header>
+
+    <main>
+
+        <?php
+            echo "Hello, World!";
+        ?>
+    </main>
+
+    <footer>
+        <h2>Домашняя работа: Hello, World!</h2>
+    </footer>
+</body>
+</html>
